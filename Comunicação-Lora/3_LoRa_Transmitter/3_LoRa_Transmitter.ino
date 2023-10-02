004ed8785e978b7b47e92b600cdf13deddef4131
Nicole Silva <nicolesilva8144@gmail.com>
2023-10-02 16:34:10 -0300

    Editando codigos protocolo

--- Comunicação-Lora/3_LoRa_Transmitter/3_LoRa_Transmitter.ino
+++ Comunicação-Lora/3_LoRa_Transmitter/3_LoRa_Transmitter.ino
@@ -1,27 +1,5 @@
-/*******************************************************************************************************
-  Programs for Arduino - Copyright of the author Stuart Robinson - 06/02/20
-
-  This program is supplied as is, it is up to the user of the program to decide if the program is
-  suitable for the intended purpose and free from errors.
-*******************************************************************************************************/
 
 
-/*******************************************************************************************************
-  Program Operation - This is a simple LoRa test transmitter. A packet containing ASCII text is sent
-  according to the frequency and LoRa settings specified in the 'Settings.h' file. The pins to access
-  the lora device need to be defined in the 'Settings.h' file also.
-
-  The details of the packet sent and any errors are shown on the Serial Monitor, together with the transmit
-  power used, the packet length and the CRC of the packet. The matching receive program, '4_LoRa_Receive'
-  can be used to check the packets are being sent correctly, the frequency and LoRa settings (in Settings.h)
-  must be the same for the Transmit and Receive program. Sample Serial Monitor output;
-
-  10dBm Packet> {packet contents*}  BytesSent,23  CRC,DAAB  TransmitTime,54mS  PacketsSent,1
-
-  Serial monitor baud rate is set at 9600
-*******************************************************************************************************/
-
-#define Program_Version "V1.0 Modificado por Lucas"
 
 #include <SPI.h>                                               //the SX128X device is SPI based so load the SPI library                                         
 #include <SX128XLT.h>                                          //include the appropriate library  
--- Comunicação-Lora/4_LoRa_Receiver/4_LoRa_Receiver.ino
+++ Comunicação-Lora/4_LoRa_Receiver/4_LoRa_Receiver.ino
@@ -1,28 +1,3 @@
-/*******************************************************************************************************
-  Programs for Arduino - Copyright of the author Stuart Robinson - 08/02/20
-
-  This program is supplied as is, it is up to the user of the program to decide if the program is
-  suitable for the intended purpose and free from errors.
-*******************************************************************************************************/
-
-/*******************************************************************************************************
-  Program Operation - The program listens for incoming packets using the LoRa settings in the 'Settings.h'
-  file. The pins to access the lora device need to be defined in the 'Settings.h' file also.
-
-  There is a printout of the valid packets received, the packet is assumed to be in ASCII printable text,
-  if its not ASCII text characters from 0x20 to 0x7F, expect weird things to happen on the Serial Monitor.
-  The LED will flash for each packet received and the buzzer will sound, if fitted.
-
-  Sample serial monitor output;
-
-  1109s  Hello World 1234567890*,CRC,DAAB,RSSI,-61dBm,SNR,9dB,Length,23,Packets,1026,Errors,0,IRQreg,50
-
-  If there is a packet error it might look like this, which is showing a CRC error,
-
-  1189s PacketError,RSSI,-111dBm,SNR,-12dB,Length,0,Packets,1126,Errors,1,IRQreg,70,IRQ_HEADER_VALID,IRQ_CRC_ERROR,IRQ_RX_DONE
-
-  Serial monitor baud rate is set at 9600.
-*******************************************************************************************************/
 
 #define Program_Version "V1.0"
 
